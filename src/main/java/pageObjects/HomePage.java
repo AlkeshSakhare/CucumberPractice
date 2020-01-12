2cee530ecc7352ce1b041e28bb500b32e5956014
AlkeshSakhare <akleshsakhare@yahoo.com>
2020-01-12 12:53:25 +0530

    CucumberDemo

--- src/main/java/pageObjects/HomePage.java
+++ src/main/java/pageObjects/HomePage.java
@@ -3,6 +3,8 @@ package pageObjects;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.support.PageFactory;
 
+import managers.FileReaderManager;
+
 public class HomePage {
 	WebDriver driver;
 
@@ -12,11 +14,12 @@ public class HomePage {
 	}
 
 	public void perform_Search(String search) {
-		driver.navigate().to("http://shop.demoqa.com/?s=" + search + "&post_type=product");
+		driver.navigate().to(FileReaderManager.getInstance().getConfigReader().getApplicationUrl() + "/?s=" + search
+				+ "&post_type=product");
 	}
 
 	public void navigateTo_HomePage() {
-		driver.get("http://www.shop.demoqa.com");
+		driver.get(FileReaderManager.getInstance().getConfigReader().getApplicationUrl());
 	}
 
 }

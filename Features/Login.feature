6d236d8a57df7c48b64bbc9d6c8b09f83181629e
AlkeshSakhare <akleshsakhare@yahoo.com>
2020-01-12 11:21:56 +0530

    CucumberDemo

--- Features/Login.feature
+++
@@ -1,7 +0,0 @@
-Feature: Visit facebook.com
-
-Scenario: View Facebook Page
-	Given chrome browser is launched
-	And User navigate to facebook.com
-	Then verify page title
-	Then close browser
---
+++ src/main/java/pageObjects/ConfirmationPage.java
@@ -0,0 +1,11 @@
+package pageObjects;
+
+import org.openqa.selenium.WebDriver;
+
+public class ConfirmationPage {
+
+	public ConfirmationPage(WebDriver driver) {
+		// TODO Auto-generated constructor stub
+	}
+
+}
